12661aa3fb2f084f3c2d1ae68fff8bec07ae3c14
lise-non <58372618+lise-non@users.noreply.github.com>
2022-01-06 11:23:48 +0100

    MAJ variables.php
    variables numérique

---
+++ table.php
@@ -0,0 +1,35 @@
+<?php
+
+$tableau = [1,2,3,4,5];
+$tableauDeCaractere = [ 'a', 'b', 'c'];
+
+print_r($tableau);
+
+echo '<pre>';
+print_r($tableau);
+echo '</pre>';
+
+$tableau[] = 6;
+
+echo '<pre>';
+print_r($tableau);
+echo '</pre>';
+
+$tableau2DImension = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+echo $tableau2DImension[1][2];
+
+//tableau associatifs (ecrit en snake_case)
+
+$professeur = [
+    'prenom' => 'arthur'
+];
+
+$phrase = '';
+$phrase .= $professeur['prenom'];
+
+echo '<p>'. $phrase. '</p>';
--- variables.php
+++ variables.php
@@ -36,3 +36,4 @@ $phrase .= $prenomDuProfesseur;
 
 echo '<br />';
 echo $phrase;
+
